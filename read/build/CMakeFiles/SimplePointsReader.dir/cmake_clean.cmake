771492d85edc24c16fd9b32b4569ed69e76d4af7
rodety <rodety@gmail.com>
2014-12-13 23:59:24 -0500

    Primer Commint

---
+++ read/build/CMakeFiles/SimplePointsReader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SimplePointsReader.dir/SimplePointsReader.cxx.o"
+  "SimplePointsReader.pdb"
+  "SimplePointsReader"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/SimplePointsReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
